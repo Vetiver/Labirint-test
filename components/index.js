a957660af4220e9d77230e6172ccf2035f1be301
Vetiver <dmitriy_serga@mail.ru>
2022-07-03 06:50:33 +0300

    комит

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 import {openPopup, closePopup, closeByEscape} from './modals.js';
-import {mobilePopup, mobileOpenButton, mobileCloseButton, firstDate, secondDate} from './utils/constants.js';
+import {mobilePopup, mobileOpenButton, mobileCloseButton, firstDate, secondDate, firstActive, secondActive} from './utils/constants.js';
 
 mobileOpenButton.addEventListener('click', evt => {
   openPopup(mobilePopup);
@@ -17,3 +17,6 @@ if (window.innerWidth <= 490) {
   firstDate.textContent = 'пн, 24 сен — ср, 26 сен';
   secondDate.textContent = 'чт, 27 сен — пт, 28 сен'
 }
+
+firstActive.style.border = '2px solid #065CC2';
+secondActive.style.border = '2px solid #065CC2'
--- components/utils/constants.js
+++ components/utils/constants.js
@@ -3,5 +3,7 @@ const mobileOpenButton = document.querySelector('.ordering__popup-button');
 const mobileCloseButton = document.querySelector('.popup__close-button');
 const firstDate = document.querySelector('#first__date');
 const secondDate = document.querySelector('#second__date');
+const firstActive = document.querySelector('#firstActive');
+const secondActive = document.querySelector('#secondActive')
 
-export {mobilePopup, mobileOpenButton, mobileCloseButton, firstDate, secondDate}
+export {mobilePopup, mobileOpenButton, mobileCloseButton, firstDate, secondDate, firstActive, secondActive}
